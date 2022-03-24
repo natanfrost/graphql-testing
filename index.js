4b1dd324849c4d7b6042081f1eeee4b17e349d4b
Diamond <nataniel.r.v@gmail.com>
2022-03-24 17:48:29 -0300

    using object types

--- index.js
+++ index.js
@@ -5,37 +5,70 @@ const { gql, ApolloServer } = require("apollo-server");
  * Int, Float, String, Boolean, ID
  */
 
+const produtos = [
+    {
+        id: 123456789,
+        nome: 'iPhone',
+        valor: 5999.00,
+        disponivel: false
+    },
+    {
+        id: 123456780,
+        nome: 'MacBook Pro',
+        valor: 15999.00,
+        disponivel: true
+    }
+]
+
+const usuarios = [
+    {
+        id: 1,
+        nome: 'fulano de tal',
+        salario: '12345.00',
+        ativo: true,
+        tecnologias: ['React', 'GraphQL']
+    },
+    {
+        id: 2,
+        nome: 'beltrano da silva',
+        salario: '82345.00',
+        ativo: false,
+        tecnologias: ['Angular', 'Vue']
+    }
+]
+
 const resolvers = {
     Query: {
-        idade() {
-            return 18;
+        usuarios() {
+            return usuarios;
         },
-        salario() {
-            return 1234.00;
-        },
-        nome() {
-            return 'Fulano de Tal';
-        },
-        ativo() {
-            return true;
-        },
-        id() {
-            return 123465789;
-        },
-        tecnologias() {
-            return ['GraphQL', 'ReactJS', 'NestJS'];
+        produtos() {
+            return produtos;
         }
     }
 }
 
 const typeDefs = gql`
-    type Query {
+
+    type Produto {
+        id: ID
+        nome: String
+        valor: Float
+        disponivel: Boolean
+    }
+
+    type Usuario {
         idade: Int
         salario: Float
         nome: String
         ativo: Boolean
-        id: ID,
+        id: ID
         tecnologias: [String!]!
+    }
+
+    type Query {
+        usuarios: [Usuario]   
+        produtos: [Produto]    
     }
 `
 
@@ -44,4 +77,4 @@ const server = new ApolloServer({
     resolvers
 })
 
-server.listen(4000);
+server.listen();
