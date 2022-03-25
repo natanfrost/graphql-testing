4fe7cab87106bab6f04a5264694bab10db7b951b
Diamond <nataniel.r.v@gmail.com>
2022-03-25 11:55:02 -0300

    refactoring code to use mocking local db

--- index.js
+++ index.js
@@ -5,77 +5,77 @@ const { gql, ApolloServer } = require("apollo-server");
  * Int, Float, String, Boolean, ID
  */
 
-const produtos = [
-    {
-        id: 123456789,
-        nome: 'iPhone',
-        valor: 5999.00,
-        disponivel: false
-    },
-    {
-        id: 123456780,
-        nome: 'MacBook Pro',
-        valor: 15999.00,
-        disponivel: true
-    }
-]
-
-const perfis = [
-    {
-        id: 1,
-        descricao: 'ADMIN'
-    },
-    {
-        id: 2,
-        descricao: 'NORMAL'
-    }
-]
-
-const usuarios = [
-    {
-        id: 1,
-        nome: 'fulano de tal',
-        salario: '12345.00',
-        ativo: true,
-        tecnologias: ['React', 'GraphQL'],
-        perfil: 1
-    },
-    {
-        id: 2,
-        nome: 'beltrano da silva',
-        salario: '82345.00',
-        ativo: false,
-        tecnologias: ['Angular', 'Vue'],
-        perfil: 2
-    }
-]
+const db = {
+    produtos: [
+        {
+            id: 123456789,
+            nome: 'iPhone',
+            valor: 5999.00,
+            disponivel: false
+        },
+        {
+            id: 123456780,
+            nome: 'MacBook Pro',
+            valor: 15999.00,
+            disponivel: true
+        }
+    ],
+    perfis: [
+        {
+            id: 1,
+            descricao: 'ADMIN'
+        },
+        {
+            id: 2,
+            descricao: 'NORMAL'
+        }
+    ],
+    usuarios: [
+        {
+            id: 1,
+            nome: 'fulano de tal',
+            salario: '12345.00',
+            ativo: true,
+            tecnologias: ['React', 'GraphQL'],
+            perfil: 1
+        },
+        {
+            id: 2,
+            nome: 'beltrano da silva',
+            salario: '82345.00',
+            ativo: false,
+            tecnologias: ['Angular', 'Vue'],
+            perfil: 2
+        }
+    ]
+}
 
 const resolvers = {    
     Usuario: {
         perfil(usuario) {
-            return perfis.find(p => p.id === usuario.id);
+            return db.perfis.find(p => p.id === usuario.id);
         }
     },
 
     Query: {
         usuarios() {
-            return usuarios;
+            return db.usuarios;
         },
         produtos() {
-            return produtos;
+            return db.produtos;
         },
         produto(_, args){
-            return produtos.find(produto => produto.nome.toUpperCase().includes(args.nome.toUpperCase())) || {};
+            return db.produtos.find(produto => produto.nome.toUpperCase().includes(args.nome.toUpperCase())) || {};
         },
         usuario(_, args) {
             const { id, nome } = args;
             if (id) {
-                return usuarios.find(usuario => usuario.id === id) || {};
+                return db.usuarios.find(usuario => usuario.id === id) || {};
             }
-            return usuarios.find(usuario => usuario.nome === nome) || {};
+            return db.usuarios.find(usuario => usuario.nome === nome) || {};
         },
         perfis() {
-            return perfis;
+            return db.perfis;
         }
     }
 }
