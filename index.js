27dde65d271b7fd4129b9f5864fd444c1d810cfa
Diamond <nataniel.r.v@gmail.com>
2022-03-25 11:14:22 -0300

    scafolding objects

--- index.js
+++ index.js
@@ -20,24 +20,43 @@ const produtos = [
     }
 ]
 
+const perfis = [
+    {
+        id: 1,
+        descricao: 'ADMIN'
+    },
+    {
+        id: 2,
+        descricao: 'NORMAL'
+    }
+]
+
 const usuarios = [
     {
         id: 1,
         nome: 'fulano de tal',
         salario: '12345.00',
         ativo: true,
-        tecnologias: ['React', 'GraphQL']
+        tecnologias: ['React', 'GraphQL'],
+        perfil: 1
     },
     {
         id: 2,
         nome: 'beltrano da silva',
         salario: '82345.00',
         ativo: false,
-        tecnologias: ['Angular', 'Vue']
+        tecnologias: ['Angular', 'Vue'],
+        perfil: 2
     }
 ]
 
-const resolvers = {
+const resolvers = {    
+    Usuario: {
+        perfil(usuario) {
+            return perfis.find(p => p.id === usuario.id);
+        }
+    },
+
     Query: {
         usuarios() {
             return usuarios;
@@ -54,6 +73,9 @@ const resolvers = {
                 return usuarios.find(usuario => usuario.id === id) || {};
             }
             return usuarios.find(usuario => usuario.nome === nome) || {};
+        },
+        perfis() {
+            return perfis;
         }
     }
 }
@@ -67,20 +89,27 @@ const typeDefs = gql`
         disponivel: Boolean
     }
 
+    type Perfil {
+        id: Int
+        descricao: String
+    }
+
     type Usuario {
         idade: Int
         salario: Float
         nome: String
         ativo: Boolean
         id: ID
-        tecnologias: [String!]!
+        tecnologias: [String!]!,
+        perfil: Perfil
     }
-
+    
     type Query {
         usuarios: [Usuario]   
         produtos: [Produto]    
         usuario(id: Int, nome: String): Usuario
         produto(nome: String): Produto
+        perfis: [Perfil]
     }
 `
 
